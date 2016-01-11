93fc021d2e78539016b768c8f536ed8750bc23f7
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-01-11 23:02:16 +0100

    PSR2 Fix for elda loader

--- public/content/mu-plugins/elda-loader.php
+++ public/content/mu-plugins/elda-loader.php
@@ -13,25 +13,25 @@ use Frozzare\Elda\Elda;
 
 // Configuration.
 $path     = __DIR__ . '/../../../app/WordPress';
-$path     = realpath( $path );
-$lib_path = $path . '/' . ( defined( 'ELDA_LIB_DIR' ) ? ELDA_LIB_DIR : 'lib' );
+$path     = realpath($path);
+$lib_path = $path . '/' . ( defined('ELDA_LIB_DIR') ? ELDA_LIB_DIR : 'lib' );
 
 // Load all library files.
-$files    = glob( $lib_path . '/*', GLOB_NOSORT );
-$files    = array_map( function ( $file ) use( $path ) {
-	$file = str_replace( $path, '', $file );
-	$file = ltrim( $file, '/' );
+$files    = glob($lib_path . '/*', GLOB_NOSORT);
+$files    = array_map(function ($file) use ($path) {
+    $file = str_replace($path, '', $file);
+    $file = ltrim($file, '/');
 
-	return $file;
-}, $files );
+    return $file;
+}, $files);
 
 /**
  * Bootstrap plugin with Elda.
  */
-Elda::boot( $path, [
-	'domain'    => ELDA_DOMAIN,
-  	'files'     => $files,
-  	'lang_path' => __DIR__ . '/../languages',
-  	'namespace' => defined( 'ELDA_NAMESPACE' ) ? ELDA_NAMESPACE : 'App\\WordPress\\',
-	'src_dir'   => ''
-] );
+Elda::boot($path, [
+    'domain'    => ELDA_DOMAIN,
+    'files'     => $files,
+    'lang_path' => __DIR__ . '/../languages',
+    'namespace' => defined('ELDA_NAMESPACE') ? ELDA_NAMESPACE : 'App\\WordPress\\',
+    'src_dir'   => ''
+]);
