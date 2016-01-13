c8e3a49b3e322e63c9daeb6b2b15247acab533c1
David Söderberg <david.soderberg@isotop.se>
2016-01-13 11:05:41 +0100

    Added corcel and google places. Created PostPageType. Removed ExamplePageType. Removed theme and page post type menu item in admin.

--- app/WordPress/lib/papi.php
+++ app/WordPress/lib/papi.php
@@ -10,3 +10,11 @@ namespace App\WordPress\Lib;
 add_filter('papi/settings/directories', function () {
     return __DIR__ . '/../PageTypes';
 });
+
+/**
+ * Make `post-page-type.php`the only page type
+ * that `post` post type can use.
+ */
+add_filter('papi/settings/only_page_type_post', function () {
+    return 'PostPageType';
+});
